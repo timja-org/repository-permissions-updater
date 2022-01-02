f72df3c411f67a186a452058e2f1ee0ea3e26a0c
Tim Jacomb <timjacomb1+github@gmail.com>
2022-01-02 09:41:56 +0000

    Add hosting files

---
+++ src/main/java/io/jenkins/infra/repository_permissions_updater/hosting/HostingConfig.java
@@ -0,0 +1,32 @@
+package io.jenkins.infra.repository_permissions_updater.hosting;
+
+import java.util.Objects;
+
+import static java.util.Objects.requireNonNull;
+
+public final class HostingConfig {
+
+    static final String TARGET_ORG_NAME;
+    static final String INFRA_ORGANIZATION;
+    static final String HOSTING_REPO_SLUG;
+    static final String HOSTING_REPO_NAME = "repository-permissions-updater";
+    static final String JIRA_URL = "https://issues.jenkins.io";
+    static final String JIRA_USERNAME = requireNonNull(System.getenv("JIRA_USERNAME"));
+    static final String JIRA_PASSWORD = requireNonNull(System.getenv("JIRA_PASSWORD"));
+    static final String JIRA_PROJECT;
+
+    private HostingConfig() {
+    }
+
+    static {
+        String orgOverride = System.getenv("ORG_NAME");
+        INFRA_ORGANIZATION = orgOverride != null ? orgOverride : "jenkins-infra";
+        HOSTING_REPO_SLUG = INFRA_ORGANIZATION + "/" + HOSTING_REPO_NAME;
+
+        String targetOrgOverride = System.getenv("TARGET_ORG_NAME");
+        TARGET_ORG_NAME = targetOrgOverride != null ? targetOrgOverride : "jenkinsci";
+
+        String projectOverride = System.getenv("JIRA_PROJECT_NAME");
+        JIRA_PROJECT = projectOverride != null ? projectOverride : "JENKINS";
+    }
+}
